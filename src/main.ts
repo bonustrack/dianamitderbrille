a017a6e72ecddcbd63a80b94cf05c10183f8d091
adcpm <fabien@bonustrack.co>
2020-01-01 12:39:59 +0700

    Fix lint

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import Vue from "vue";
+import Vue from 'vue';
 import { upperFirst, camelCase } from 'lodash';
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
 const requireComponent = require.context('./components', true, /[\w-]+\.vue$/);
 requireComponent.keys().forEach(fileName => {
@@ -17,4 +17,4 @@ new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app');

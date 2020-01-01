190969f6e07eb6d1815b86f5c61c9cc9d99bf906
adcpm <fabien@bonustrack.co>
2020-01-01 12:34:43 +0700

    Add auto-import for components

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import Vue from "vue";
+import { upperFirst, camelCase } from 'lodash';
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+
+const requireComponent = require.context('./components', true, /[\w-]+\.vue$/);
+requireComponent.keys().forEach(fileName => {
+  const componentConfig = requireComponent(fileName);
+  const componentName = upperFirst(camelCase(fileName.replace(/^\.\//, '').replace(/\.\w+$/, '')));
+  Vue.component(componentName, componentConfig.default || componentConfig);
+});
 
 Vue.config.productionTip = false;
 

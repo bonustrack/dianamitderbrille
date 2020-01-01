f545c3404948f5bec66be3538da0ef8fe58bd3cb
adcpm <fabien@bonustrack.co>
2020-01-01 16:34:20 +0700

    Add express start script

---
+++ start.ts
@@ -0,0 +1,13 @@
+const express = require('express');
+const frameguard = require('frameguard');
+const serveStatic = require('serve-static');
+
+const app = express();
+app.use(frameguard({ action: 'deny' }));
+app.use(serveStatic(`${__dirname}/dist`));
+app.get('*', (req, res) => res.sendFile(`${__dirname}/dist/index.html`));
+
+const port = process.env.PORT || 5000;
+const server = app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});

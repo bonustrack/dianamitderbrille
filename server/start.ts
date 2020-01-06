4d53713b471f88b9cce58241517d8637e6b249ae
adcpm <fabien@bonustrack.co>
2020-01-06 11:20:45 +0700

    Fix path to dist

--- server/start.ts
+++ server/start.ts
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(serveStatic(`${__dirname}/dist`));
 
 app.use('/api', api);
-app.get('*', (req, res) => res.sendFile(`${__dirname}/dist/index.html`));
+app.get('*', (req, res) => res.sendFile(`${__dirname}/../dist/index.html`));
 
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {

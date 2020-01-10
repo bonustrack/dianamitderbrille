f4f434295184c684c3e66faab02fc4df5ba2c1fc
adcpm <fabien@bonustrack.co>
2020-01-10 14:53:12 +0700

    Add textarea

--- src/components/Write.vue
+++ src/components/Write.vue
@@ -1,10 +1,24 @@
 <template>
-  <div class="border-bottom d-flex">
-    <Upload v-model="file" class="p-4 border-right">
-      <div class="iconfont iconimage overflow-hidden" />
-    </Upload>
-    <div class="flex-auto text-right py-3 px-4">
-      <button class="btn btn-primary" @click="handleSubmit">Post</button>
+  <div>
+    <div class="border-bottom d-flex">
+      <div class="ml-5">
+        <Avatar class="mr-3" />
+      </div>
+      <div class="d-flex flex-column flex-auto">
+        <textarea
+          class="form-control flex-auto width-full mb-0"
+          style="height: 64px;"
+          placeholder="Compose new post..."
+        />
+        <div class="d-flex ove">
+          <Upload v-model="file" class="p-3 border-right">
+            <div class="iconfont iconimage overflow-hidden" />
+          </Upload>
+          <div class="flex-auto text-right py-2 px-4">
+            <button class="btn btn-primary" @click="handleSubmit">Post</button>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
